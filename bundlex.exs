741449ccb1fce6b284a0838c9bf3ac91c41ce26e
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-10-03 12:29:33 +0200

    Use precompiled libnice

--- bundlex.exs
+++ bundlex.exs
@@ -7,12 +7,31 @@ defmodule ExLibnice.BundlexProject do
     ]
   end
 
+  defp get_libnice_url() do
+    case Bundlex.get_target() do
+      %{os: "linux"} ->
+        {:precompiled,
+         "https://github.com/membraneframework-precompiled/precompiled_libnice/releases/latest/download/libnice_linux.tar.gz"}
+
+      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
+        {:precompiled,
+         "https://github.com/membraneframework-precompiled/precompiled_libnice/releases/latest/download/libnice_macos_intel.tar.gz"}
+
+      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
+        {:precompiled,
+         "https://github.com/membraneframework-precompiled/precompiled_libnice/releases/latest/download/libnice_macos_arm.tar.gz"}
+
+      _other ->
+        nil
+    end
+  end
+
   defp natives() do
     [
       native: [
         sources: ["native.c", "parser.c"],
         deps: [unifex: :unifex],
-        pkg_configs: ["nice"],
+        os_deps: [{get_libnice_url(), "nice"}],
         interface: [:nif, :cnode],
         preprocessor: Unifex
       ]
