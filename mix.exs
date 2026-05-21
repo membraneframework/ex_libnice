74828b67b93fdbc2429d6c8ba4463ef7aa258da7
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2026-05-21 17:07:20 +0200

    Fix Apache license SPDX identifier format
    Change 'Apache 2.0' to 'Apache-2.0' to use the correct SPDX identifier.

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule ExLibnice.MixProject do
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
