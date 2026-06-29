c95820891556299b5a9fcd09bc040b685a2f99de
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:25:37 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule ExLibnice.MixProject do
       {:unifex, "~> 1.0"},
       {:bunch, "~> 1.3.0"},
       {:mdns, "~> 1.0.12"},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
